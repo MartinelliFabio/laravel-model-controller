15842a8cf8e8e6df65687290b70989ea326085d7
MartinelliFabio <fabio.martinelli996@gmail.com>
2022-12-16 17:41:29 +0100

    Created header section

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,7 @@
 <header>
-    fdgswfdfrwhf
+    <nav class="navbar">
+        <div class="container-fluid">
+            <span class="mb-0 display-6 fw-bold">MoviesBool</span>
+        </div>
+    </nav>
 </header>
